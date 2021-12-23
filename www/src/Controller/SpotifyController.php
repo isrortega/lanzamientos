1a698956697cb28489f6493f8d9e5e2da0a3f71b
Ismael Romero Ortega <iromero@araujoysegovia.net>
2021-12-23 13:10:48 -0500

    Documentando controlador

--- www/src/Controller/SpotifyController.php
+++ www/src/Controller/SpotifyController.php
@@ -6,11 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
- * Clase para intercambiar datos con spotify y 
+ * Clase para intercambiar entre el servicio de spotify y las vista
  */
 class SpotifyController extends AbstractController {
 
 
+    /**
+     * Con este método solicitamos al servicio de Spotify que nos suministre
+     * los lanzamientos recientes para renderizarlos en una plantilla twig
+     *
+     * @param Spotify $spotify Servicio de Spotify
+     * @return Response 
+     */
     public function lanzamientos(Spotify $spotify) : Response {
         $new_releases = $spotify->getLanzamientos();
 
