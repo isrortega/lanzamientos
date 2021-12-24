c17f48f6d42009c35ea11b33a5cd86f3a32d2997
Ismael Romero Ortega <iromero@araujoysegovia.net>
2021-12-24 10:31:05 -0500

    Mejorada la interfaz gráfica - Se extiende de base.html.twig - Se mejora la apariencia de lanzamientos y artistas con Tailwind - Se agrega un paginador para los lanzamientos

--- www/src/Controller/SpotifyController.php
+++ www/src/Controller/SpotifyController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Service\Spotify;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Clase para intercambiar entre el servicio de spotify y las vista
@@ -15,14 +16,17 @@ class SpotifyController extends AbstractController {
      * Con este método solicitamos al servicio de Spotify que nos suministre
      * los lanzamientos recientes para renderizarlos en una plantilla twig
      *
+     * @param Request $request  La data del request actual
      * @param Spotify $spotify Servicio de Spotify
      * @return Response 
      */
-    public function lanzamientos(Spotify $spotify) : Response {
-        $new_releases = $spotify->getNewReleases();
+    public function lanzamientos(Request $request, Spotify $spotify) : Response {
+        $page = $request->query->get('p');
+        $new_releases = $spotify->getNewReleases($page);
 
         return $this->render('spotify/lanzamientos.html.twig', [
-            'albumns' => $new_releases->albums->items,
+            'albumns' => $new_releases->data->albums->items,
+            'page' => $new_releases->page->number,
         ]);
     } // lanzamientos
 
@@ -31,13 +35,17 @@ class SpotifyController extends AbstractController {
      * Con este método solicitamos al servicio de Spotify que nos suministre
      * los datos del artista y el listado de sus top_tracks
      *
+     * @param Request $request  La data del request actual
      * @param Spotify $spotify  Servicio de Spotify
      * @param String|null $artist_id El id del artista
      * @return Response
      */
-    public function artista(Spotify $spotify, String $artist_id = null) : Response {
+    public function artista(Request $request, Spotify $spotify, String $artist_id = null) : Response {
         $top_tracks = null;
         $artist_error = null;
+        $page = $request->query->get('p');
+        if(is_null($page))
+            $page = 1;
         
         if(is_null($artist_id)){
             // todo retornar a página de artista no válido
@@ -54,6 +62,7 @@ class SpotifyController extends AbstractController {
             'artist' => $artist,
             'top_tracks' => ((isset($top_tracks->tracks)) ? $top_tracks->tracks : null),
             'artist_error' => $artist_error,
+            'page' => $page,
         ]);
     } // artista
 
